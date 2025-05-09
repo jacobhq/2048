c852030ffe79e85f0c36092ef4631acfeeef88f0
Jacob Marshall <me@jhqcat.com>
2025-05-09 07:54:11 +0100

    game now feature complete

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using UnityEngine;
+using TMPro;
 
 public class GameManager: MonoBehaviour
 {
     public CanvasGroup gameOver;
     public TileBoard board;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI highScoreText;
+
+    private int score;
 
     private void Start()
     {
@@ -13,6 +18,9 @@ public class GameManager: MonoBehaviour
 
     public void NewGame()
     {
+        SetScore(0);
+        highScoreText.text = LoadHighScore().ToString();
+
         gameOver.alpha = 0f;
         gameOver.interactable = false;
 
@@ -46,5 +54,33 @@ public class GameManager: MonoBehaviour
         }
 
         canvasGroup.alpha = to;
+    }
+
+    public void IncreaseScore(int points)
+    {
+        SetScore(score + points);
+    }
+
+    private void SetScore(int score)
+    {
+        this.score = score;
+        scoreText.text = score.ToString();
+
+        SaveHighScore();
+    }
+
+    private void SaveHighScore()
+    {
+        int highScore = LoadHighScore();
+
+        if (score > highScore)
+        {
+            PlayerPrefs.SetInt("high_score", highScore);
+        }
+    }
+
+    private int LoadHighScore()
+    {
+        return PlayerPrefs.GetInt("high_score", 0);
     }
 }
